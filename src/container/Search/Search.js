a03a7aa47c2aa3a909c2e19574923bf474b9d9f1
ishraqe <kanak431@diu.edu.bd>
2018-12-24 22:01:57 +0600

    search page done

--- src/container/Search/Search.js
+++ src/container/Search/Search.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Header from "../../component/Header";
 import HeadingTitle from "../../component/HeadingTitle";
 import Button from "../../component/Button";
-import { HelpIcon } from "../../component/svgIcon";
+import { HelpIcon, DownArrowIcon } from "../../component/svgIcon";
 import { Helmet } from "react-helmet";
 export default class SearchContainer extends Component {
   render() {
@@ -13,19 +13,81 @@ export default class SearchContainer extends Component {
         </Helmet>
         <Header {...this.props} info={["home", "search house"]} />
         <HeadingTitle title="search house" />
-        <div classname="searchContainer">
+        <div className="section-search">
           <div className="container">
-            <form action="#">
-              <div className="wrapper">
-                <div className="col-1-of-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-                  tempore!
-                </div>
-                <div className="col-1-of-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-                  tempore!
-                </div>
-              </div>
+            <form action="#" className="search">
+              <ul className="search__list">
+                <li className="search__item">
+                  <h2 className="heading-secondary-sub">
+                    Location / property name
+                  </h2>
+                  <div className="search__input-container">
+                    <input
+                      type="text"
+                      className="search__input"
+                      placeholder="Type by location, property name"
+                    />
+                    <HelpIcon className="search__help__icon" />
+                  </div>
+                </li>
+                <li className="search__item">
+                  <h2 className="heading-secondary-sub">Housing Type</h2>
+                  <div className="search__input-container">
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        placeholder="Housing Type"
+                        className="input"
+                        // disabled={props.showProperty}
+                      />
+                      <a>
+                        <DownArrowIcon className="icon down-icon" />
+                      </a>
+
+                      <div className="expendadInput">
+                        <label className="ex-container">
+                          Landed
+                          <input type="checkbox" value="Landed" />
+                          <span className="checkmark" />
+                        </label>
+                        <label className="ex-container">
+                          High rise
+                          <input type="checkbox" value="High Rise" />
+                          <span className="checkmark" />
+                        </label>
+                      </div>
+                    </div>
+
+                    <HelpIcon className="search__help__icon" />
+                  </div>
+                </li>
+                <li className="search__item">
+                  <h2 className="heading-secondary-sub">Price (RM)</h2>
+                  <div className="search__input-container">
+                    <div className="search_range-container">
+                      <input
+                        type="text"
+                        className="search_range__input"
+                        placeholder="0"
+                      />
+                      <span className="search_range-text">to</span>
+                      <input
+                        type="text"
+                        className="search_range__input"
+                        placeholder="5000"
+                      />
+                    </div>
+                  </div>
+                </li>
+                <li className="search__item">
+                  <h2 className="heading-secondary-sub"> &nbsp; </h2>
+                  <div className="search__input-container">
+                    <a type="submit" href="#" className="btn-listing">
+                      Search
+                    </a>
+                  </div>
+                </li>
+              </ul>
             </form>
           </div>
         </div>
